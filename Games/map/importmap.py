72d11758c95d3078d637d0c9b9dcc316a8240cfc
Elie Zelaznik <elie.zelaznik@epita.fr>
2026-01-13 14:40:30 +0100

    debug maniamé

--- Games/map/importmap.py
+++ Games/map/importmap.py
@@ -7,6 +7,7 @@ class GameMap:
     def __init__(self,screen_size):
         self.tmx_data = load_pygame('Games/Resssource/map/map.tmx')
         self.map_layer = pyscroll.BufferedRenderer(pyscroll.data.TiledMapData(self.tmx_data), screen_size)
+        self.map_layer.zoom = 1
         
     def render(self, surface, center):
         self.map_layer.center(center)
